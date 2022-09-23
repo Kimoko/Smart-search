46560dcdf904ee86c141a2240975a5670c10c0e2
Kirill <46571896+Kimoko@users.noreply.github.com>
2022-09-23 21:10:29 +0500

    руты

---
+++ client/src/pages/buyer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const buyer = () => {
+  return (
+    <div >
+      покупатель
+    </div>
+  );
+};
+
+export default buyer;
---
+++ client/src/pages/sales.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const sales = () => {
+  return (
+    <div >
+      продавец
+    </div>
+  );
+};
+
+export default sales;
