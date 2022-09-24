9ab59b2f212e0b7a1f58ef561688a82fb8a0f75b
Kirill <46571896+Kimoko@users.noreply.github.com>
2022-09-24 12:06:00 +0500

    last com

--- client/src/components/CompositionItem.js
+++ client/src/components/CompositionItem.js
@@ -23,12 +23,13 @@ const CompositionItem = ({produts}) => {
             
             <div class="col" onClick={() => history.push(BUYER_ROUTE + '/' + produts.id)}>
                 <div class="card" style={{width:'100%'}} >
-               <img src={produts.imageUrl} class="card-img-top" alt="Image"/>
+               <img src={produts.imageUrl} class="card-img-top" style={{width: '90%'}} alt="Image"/>
                 <div class="card-body">
                 <div>{produts.id}</div>
                     <h6 class="card-title">{produts.name}</h6>
                     {produts.comp}
-                    <p class="card-text">{produts.price}</p> 
+                    <p class="card-text">{produts.price}</p>
+                    <h5 class="card-text" style={{textAlign:"center"}}>~{produts.сkl}~</h5>
                     <a href="#" class="btn btn-primary" style={{marginLeft: '20%'}}>Перейти к товару</a>            
                 </div>
                 </div>
--- client/src/store/produtsStore.js
+++ client/src/store/produtsStore.js
@@ -5,28 +5,22 @@ export default class ProdutsStore{
 
         this._produts=[
             {
-                "name":"Бензопилы,Электропилы"
-                ,"price":"4 000 руб", "imageUrl":"https://pimg2.supl.biz/media/thumbnails/proposals/9e/e4/supl_9ee42225-fb83-4696-b4dc-c3b7baa40dda_th_220x136.jpg",
-                "url": "https://supl.biz/benzopilyi-elektropilyi-p9545004/"
-                ,"comp":"Механик, сервисный центр"
-            },
-            {
                 "name":"Мотобур (Бензобур) Hitachi DA200E"
                 ,"price":"19 999 руб.", "imageUrl":"https://pimg1.supl.biz/media/thumbnails/proposals/images/19/88/supl_198809c2-f30c-4af6-b0e4-0b7e47fdbda6_th_220x136.jpg",
                 "url": "https://supl.biz/motobur-benzobur-hitachi-da200e-p9537952/"
-                ,"comp":"ИП Макаренков Никита Игоревич"
+                ,"comp":"ИП Макаренков Никита Игоревич", "сkl":"на складе"
             },
             {
                 "name":"Мотобур Champion AG243"
                 ,"price":"9 990 руб", "imageUrl":"https://pimg2.supl.biz/media/thumbnails/proposals/60/dc/supl_60dcc076-e4dd-45da-aee2-9c6d091e767f_th_220x136.jpg",
                 "url": "https://supl.biz/motobur-champion-ag243-p11344308/"
-                ,"comp":"Гранд Инструмент"
+                ,"comp":"Гранд Инструмент", "сkl":"на складе"
             },
             {
                 "name":"Мотобур Champion AG352"
                 ,"price":"8 600 руб", "imageUrl":"https://pimg4.supl.biz/media/thumbnails/proposals/31/7d/supl_317df2dd-e710-4030-a171-e65d0579d1fe_th_220x136.jpg",
                 "url": "https://supl.biz/motobur-champion-ag243-p11344308/"
-                ,"comp":"Гранд Инструмент"
+                ,"comp":"Гранд Инструмент", "сkl":"на стороннем складе"
             },
            
 
